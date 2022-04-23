1f73593265084730d256833ae73dab5c2be5ead3
nikcotta <nikcotta@gmail.com>
2022-04-22 17:10:58 -0700

    some minor movement completed

--- Assets/Scripts/Player1Controller.cs
+++ Assets/Scripts/Player1Controller.cs
@@ -1,18 +1,32 @@
+/*
+ * Author(s): Niklaas Cotta
+ * Last updated: 04/22/22
+ * Controller for Player 1 game object.
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class Player1Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CharacterController characterController;
 
-    // Update is called once per frame
+    public float speed = 5.0f;
+
+    private Vector3 direction;
+    private float horizontal;
+    private float vertical;
+
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            characterController.Move(direction * speed * Time.deltaTime);
+        }
     }
 }
